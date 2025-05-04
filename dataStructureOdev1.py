c954792bb35a3bd9d2b126b5b0ca176f76ebc761
ednurisk <93641570+ednurisk@users.noreply.github.com>
2025-05-04 03:00:02 +0300

    Add files via upload

---
+++ dataStructureOdev1.py
@@ -0,0 +1,24 @@
+#Insertion Sort Projesi
+
+dizi=[22,27,16,2,18,6] 
+#Yukarı verilen dizinin insertion sort türüne göre aşamalarını yazınız.
+#[22 | 27, 16, 2, 18, 6]
+#[22, 27 | 16, 2, 18, 6]
+#[16, 22, 27 | 2, 18, 6]
+#[2, 16, 22, 27 | 18, 6]
+#[2, 16, 18, 22, 27 | 6]
+#[2, 6, 16, 18, 22, 27]
+
+#Big-O gösterimini yazınız.
+#O(n²)'dir
+
+#Time Complexity: Dizi sıralandıktan sonra 18 sayısı aşağıdaki case'lerden hangisinin kapsamına girer?
+#Average Case. Çünkü aradığımız sayı dizinin ortasında
+
+
+dizi2= [7,3,5,8,2,9,4,15,6] 
+#Yukarı verilen dizinin selection sort türüne göre ilk 4 aşamasını yazınız.
+#[2,3,5,8,7,9,4,15,6] 
+#[2,3,5,8,7,9,4,15,6] 
+#[2,3,4,8,7,9,5,15,6] 
+#[2,3,4,5,7,9,8,15,6] 

4970d545beb5073f8281e6444a2aca6507aab98a
ednurisk <93641570+ednurisk@users.noreply.github.com>
2025-05-04 03:06:26 +0300

    Add files via upload

---
+++ dataStructureOdev2.py
@@ -0,0 +1,14 @@
+#Merge Sort Projesi
+
+dizi=[16,21,11,8,12,22]
+#Yukarı verilen dizinin merge sort türüne göre aşamalarını yazınız.
+#[16, 21, 11] ve [8, 12, 22]
+#[16] ve [21, 11] VE [8] ve [12, 22]
+#[16] ve [11, 21] VE [8] ve [12, 22]
+#[11, 16, 21] VE [8, 12, 22]
+#[8, 11, 12, 16, 21, 22]
+
+
+#Big-O gösterimini yazınız.
+#O(nlogn)'dir
+
